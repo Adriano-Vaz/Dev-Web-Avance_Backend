9d4ec28e1236363e9eddb7328637ab8a66427926
DKTClementSZW <clement.szwechowiez@decathlon.com>
2024-03-21 12:08:22 +0100

    feat(light): add light db methods

--- src/main/java/fr/cszw/td_devwebavance/controllers/LightController.java
+++ src/main/java/fr/cszw/td_devwebavance/controllers/LightController.java
@@ -1,26 +1,45 @@
 package fr.cszw.td_devwebavance.controllers;
 
 import fr.cszw.td_devwebavance.models.Light;
+import fr.cszw.td_devwebavance.repositories.LightRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/lights")
+@RequiredArgsConstructor
 public class LightController {
 
+    private final LightRepository lightRepository;
+
     @GetMapping
-    public ResponseEntity<Light> getLight() {
+    public ResponseEntity<List<Light>> getLights() {
+        List<Light> lights = this.lightRepository.findAll();
+        return new ResponseEntity<>(lights, HttpStatus.OK);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Light> postLight() {
         Light light = new Light();
-        light.setId(0L);
         light.setColor("#FFFFFF");
         light.setTitle("Test");
-        light.setToggled(false);
+        light.setToggled(true);
 
-        return new ResponseEntity<>(light, HttpStatus.OK);
-
+        try {
+            Light lightCreated = this.lightRepository.save(light);
+            return new ResponseEntity<>(lightCreated, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
