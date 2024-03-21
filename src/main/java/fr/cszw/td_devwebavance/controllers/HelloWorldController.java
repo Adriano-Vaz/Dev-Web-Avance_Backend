937aabf7968825a179e7dab3661abc8db791c3e4
DKTClementSZW <clement.szwechowiez@decathlon.com>
2024-03-21 11:38:36 +0100

    feat(route): add first routes

---
+++ src/main/java/fr/cszw/td_devwebavance/controllers/HelloWorldController.java
@@ -0,0 +1,23 @@
+package fr.cszw.td_devwebavance.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+
+    @GetMapping()
+    public ResponseEntity<String> helloWorld() {
+        return new ResponseEntity<>("Hello-World !", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/2")
+    public String helloWorld2() {
+        return "Hello-World !";
+    }
+
+}
